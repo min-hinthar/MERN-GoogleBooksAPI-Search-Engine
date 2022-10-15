df76ff68317efb134d6cf7e36fc59e846cec683e
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-10-14 19:46:03 -0700

    debugging client/src/components/LoginForm and /SearchBooks, server/utils/auth.js fixed Query me errors

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -33,17 +33,11 @@ const LoginForm = () => {
     }
 
     try {
-      const {response} = await loginUser({
+      const {data} = await loginUser({
         variables: { ...userFormData },
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(response.loginUser.token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -73,7 +73,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {
+          input: bookToSave,
+        },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
