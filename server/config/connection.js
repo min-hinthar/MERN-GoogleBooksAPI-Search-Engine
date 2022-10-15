0981393121d773ca714d80a9108904d8375b89f3
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-10-15 02:02:58 -0700

    debugging heroku deployment error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:@4757Durfee91732@cluster0.0p0o2by.mongodb.net/googlebooks?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:%404757Durfee91732@cluster0.0p0o2by.mongodb.net/googlebooks?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
